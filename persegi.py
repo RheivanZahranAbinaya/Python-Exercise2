428e599c24600f64e438d00dbda34e041cca5efc
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-10-08 09:22:32 +0700

    create aplikasi persegi

---
+++ persegi.py
@@ -0,0 +1,11 @@
+print('='*40)
+print(' RUMUS PERSEGI')
+print('='*40)
+
+sisi = int(input('sisi: '))
+
+luas = sisi * sisi
+keliling = 4 * sisi
+
+print('luas : ', luas)
+print('keliling : ', keliling)
