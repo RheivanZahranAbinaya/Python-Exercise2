490b3c36e593da9cbb9ca7a104138fb69f0727be
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-10-25 07:33:59 +0700

    Kubus.py

---
+++ kubus.py
@@ -0,0 +1,13 @@
+print("="*40)
+print("PROGRAM KUBUS")
+print("="*40)
+
+sisi = float(input("masukan sisi : "))
+
+sisi = 21
+
+V : sisi * sisi * sisi # type: ignore
+LP = 4 * (sisi * sisi)
+
+print("Volume : ", V , "cm3")
+print("Luas Permukaan : ", LP , "cm2")
