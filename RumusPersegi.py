414f751cc9ed43268c8f6ae4172a64fc09c4219b
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-10-25 08:25:56 +0700

    RumusPersegi.py

---
+++ RumusPersegi.py
@@ -0,0 +1,14 @@
+# RHEIVAN ZAHRAN ABINAYA
+# KELAS X PPLG 2
+# 25 Jum'at October 2024, JAM = 08:26
+# RUMUS PERSEGI
+
+def persegi():
+
+    s = int(input("Masukan sisi persegi : "))
+
+    l = lambda s : s*s
+    k = lambda s : 4*s
+
+    print(F'''luas = {l(s)}m^2
+keliling = {k(s)}m^2''')
