223c6f857ff608890d010adde531402485ef7cd2
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-11-07 14:56:58 +0700

    LampuLaluLintas.py

---
+++ LampuLaluLintas.py
@@ -0,0 +1,10 @@
+warna = input("Masukan warna lampu lalu lintas : ").lower()
+                          
+if warna == 'merah':
+    print('Berhenti')
+elif warna == 'kuning':
+    print('siap-siap, nge gaspol MUAHAHAHAHA')
+elif warna == 'hijau':
+    print('VECHILES GO BRRRRRRRRRRRRRRRRRRRRRRR/Ehem maksud saya go XD')
+else:
+    print('Salah!')
