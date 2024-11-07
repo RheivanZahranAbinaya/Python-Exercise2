7a05e905332dd4e29b77796fe9895459d011091a
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-11-07 14:54:02 +0700

    PraktekUjian5.py

---
+++ PraktekUjian5.py
@@ -0,0 +1,10 @@
+# buatlah program untuk menjawab pertanyaan abcde jawaban yang benar
+
+s1 = 'Berapa Harga RTX TI 4090 16GB, Di toko official Nvidia :O'
+j = '25jt'
+print(s1)
+jawaban = input('jawaban: ')
+if jawaban == j:
+    print('Jawaban Benar')
+else:
+    print('Jawaban Salah')
