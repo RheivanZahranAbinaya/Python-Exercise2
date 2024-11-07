f59b24f119eb9e6034d94b95da42d16a39016c55
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-11-07 14:47:54 +0700

    Program_Lingkaran.py

---
+++ program_lingkaran.py
@@ -0,0 +1,11 @@
+print('='*40)
+print('PROGRAM LINGKARAN')
+print('='*40)
+
+PHI = 3.14
+r =float(input("masukan jari-jari lingkaran: "))
+luas = PHI*r*r
+keliling = (2*3.14*r)
+
+print("Luas lingkaran adalah : "+ str(luas))
+print("keliling lingkaran : "+ str(keliling))
