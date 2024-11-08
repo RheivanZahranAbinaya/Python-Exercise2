57e76f169f8fee9543bcdc081346e08211717a18
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-11-08 08:27:21 +0700

    Looping1.py

---
+++ Looping1.py
@@ -0,0 +1,43 @@
+#LOOPING :P
+
+nilai = 1
+
+while nilai < 6:
+    print(nilai)
+    nilai = nilai+1
+
+n = 1
+
+while n < 4:
+    print(f'''{n}-Hello''', end=" ")
+    n = n+1
+print()
+nn = 3
+while nn > 0:
+    print(f'''{nn}-Hello''', end=" ")
+    nn = nn-1
+print()
+g = 1
+
+while g <6:
+    print(f'''{g}-Hello''', end=" ")
+    g = g+2
+print()
+
+ggg = 0
+
+while ggg <7:
+    print(f'''{ggg}-Hello''', end=" ")
+    ggg = (ggg+1)*2
+
+print()
+ggs = 10
+while ggs <101:
+    print(ggs, end=" ")
+    ggs = ggs + 10
+
+print()
+ggo = 2000
+while ggo <2006:
+    print(ggo, end=" ")
+    ggo = ggo + 1
