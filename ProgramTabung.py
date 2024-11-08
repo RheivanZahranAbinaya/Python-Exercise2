7ef147d045b076587788cf39828db20ce7fea32e
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-11-08 08:47:06 +0700

    ProgramTabung.py

---
+++ ProgramTabung.py
@@ -0,0 +1,13 @@
+print('='*15)
+print('PROGRAM TABUNG')
+print('='*15)
+
+r = float(input("Masukkan Jari-Jari: "))
+t = float(input('Masukan Tinggi: '))
+luas = 2 * 3.14 * r**2
+keliling = 2 * 3.14 * r
+volume = 3.14 * r**2 * t
+
+print('Keliling\t: ' , keliling, 'Cm')
+print('Luas\t\t: ' , luas, 'Cm2')
+print('Volume\t\t: ' , volume, 'Cm3')
