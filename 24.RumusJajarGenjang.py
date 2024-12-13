c52b4334a4ab10f849bd01996ddd9ca1c5359e19
Rheivan Zahran Abinaya <rheivanabang@gmail.com>
2024-12-13 19:40:25 +0700

    24.RumusJajarGenjang.py

---
+++ 24.RumusJajarGenjang.py
@@ -0,0 +1,12 @@
+print('='*25)
+print('PROGRAM JAJAR GENJANG')
+print('='*24)
+
+alas = float(input('Masukan Alas: '))
+tinggi = float(input('Masukan Tinggi: '))
+sisimiring = float(input('Masukan Sisi Miring: '))
+luas = alas * tinggi
+keliling = 2 * (alas + tinggi)
+
+print('Luas\t\t: ' , luas, 'Cm2')
+print('Keliling\t: ' , keliling, 'Cm')
