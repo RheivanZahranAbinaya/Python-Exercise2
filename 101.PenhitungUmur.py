e41aa890b023daa1f5a2ea0135191b70727cabb6
Rheivan <rheivanabang@gmail.com>
2025-01-05 22:02:52 +0700

    Update 101.PenhitungUmur.py

--- 101.PenhitungUmur.py
+++ 101.PenhitungUmur.py
@@ -6,6 +6,6 @@ def calculate_age(birthdate):
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return age
 
-birthdate = input("Masukkan tanggal lahir (dd-mm-yyyy): ")
+birthdate = iput("Masukkan tanggal lahir (dd-mm-yyyy): ")
 age = calculate_age(birthdate)
 print(f"Umur Anda: {age} tahun")
