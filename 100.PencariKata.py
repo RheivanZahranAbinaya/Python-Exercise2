71eacecf3c05e1f075493dc3403f9cd0b3a4f2a9
Rheivan <rheivanabang@gmail.com>
2025-01-05 22:08:22 +0700

    Update 100.PencariKata.py

--- 100.PencariKata.py
+++ 100.PencariKata.py
@@ -1,9 +1,9 @@
 def find_word_in_text(word, text):
     return word in text
 
-text = inut("Masukkan teks: ")
+text = input("Masukkan teks: ")
 word = input("Masukkan kata yang ingin dicari: ")
 if find_wordin_text(word, text):
     print(f"Kata '{word}' ditemukan dalam teks.")
 else:
-    print(f"Kata '{word}' tidak ditemukan dala teks.")
+    print(f"Kata '{word}' tidak ditemukan dalam teks.")
